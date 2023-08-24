08ae2e3743fc910ceecfc0aef4f1c8b01eb0cdc4
Lukas Schneider <82098944+DryPreference@users.noreply.github.com>
2023-08-24 16:12:28 +0200

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,10 +1,10 @@
 import Lucode
 
-print("\nBeta Lucode V0.3 -- To exit type 'quit' \nThis is a personal project if you have any suggestions please contact me at: lukas.lschn.schneider@fau.de\n")
+print("\nBeta Lucode V0.4 -- To exit type 'quit' \nThis is a personal project if you have any suggestions please contact me at: lukas.lschn.schneider@fau.de\n")
 
 while True:
-    text = input('(Beta)lucode_V0.3 --> ')
-    result, error = Lucode.run('<stdin>', text)
+    text = input('(Beta)lucode_V0.4 --> ')
+    result, error = Lucode.run(' <stdin>', text)
 
     if text == 'quit' :
         break
